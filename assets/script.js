8722754b527704045b50f67a8036d6ea829109a9
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2018-11-16 18:31:49 -0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,141 @@
 var correct = 0;
 var incorrect = 0;
 
-$(document).ready(function() {
-  console.log("HELLLO");
-  $("input").on("click", function() {
-    if ($(this).hasClass("guessed")) {
-      $(this).removeClass("guessed");
-    } else {
-      $(this).addClass("guessed");
-    }
-  });
+$("#right").text(correct);
+$("#wrong").text(incorrect);
 
-  //This is where we could loop through and see the amount of elements that have both guessed and correct
-  $("submit").on("submit", function(e) {
-    e.preventDefault();
-    $("input").each(function() {
-      if ($(this).hasClass("guessed") && $(this).hasClass("correct")) {
-        correct += 1;
-      }
-    });
-    alert(
-      `This is what you got right: ${correct}. This is what you got wrong: ${4 -
-        correct}`
-    );
-  });
+$(".submit").on("click", function submitAnswer() {
+  var radios = document.getElementsByName("major-scale");
+  var i = 0,
+    len = radios.length;
+  var checked = false;
+  var userAnswer;
+
+  for (; i < len; i++) {
+    if (radios[i].checked) {
+      checked = true;
+      userAnswer = radios[i].value;
+    }
+  }
+  // Correct answer
+  if (userAnswer === "two") {
+    correct++;
+    $("#right").text(correct);
+  }
+  // incorrect answer
+  else {
+    incorrect++;
+    $("#wrong").text(incorrect);
+  }
+});
+//question 2
+$(".submit").on("click", function submitAnswer() {
+  var radios = document.getElementsByName("harmonic-minor-scale");
+  var i = 0,
+    len = radios.length;
+  var checked = false;
+  var userAnswer;
+
+  for (; i < len; i++) {
+    if (radios[i].checked) {
+      checked = true;
+      userAnswer = radios[i].value;
+    }
+  }
+  // Correct answer
+  if (userAnswer === "three") {
+    correct++;
+    $("#right").text(correct);
+  }
+  // incorrect answer
+  else {
+    incorrect++;
+    $("#wrong").text(incorrect);
+  }
+});
+//question 3
+$(".submit").on("click", function submitAnswer() {
+  var radios = document.getElementsByName("octave");
+  var i = 0,
+    len = radios.length;
+  var checked = false;
+  var userAnswer;
+
+  for (; i < len; i++) {
+    if (radios[i].checked) {
+      checked = true;
+      userAnswer = radios[i].value;
+    }
+  }
+  // Correct answer
+  if (userAnswer === "twelve") {
+    correct++;
+    $("#right").text(correct);
+  }
+  // incorrect answer
+  else {
+    incorrect++;
+    $("#wrong").text(incorrect);
+  }
+});
+//question 4
+$(".submit").on("click", function submitAnswer() {
+  var radios = document.getElementsByName("interval");
+  var i = 0,
+    len = radios.length;
+  var checked = false;
+  var userAnswer;
+
+  for (; i < len; i++) {
+    if (radios[i].checked) {
+      checked = true;
+      userAnswer = radios[i].value;
+    }
+  }
+  // Correct answer
+  if (userAnswer === "minor-third") {
+    correct++;
+    $("#right").text(correct);
+  }
+  // incorrect answer
+  else {
+    incorrect++;
+    $("#wrong").text(incorrect);
+  }
+});
+//question 5
+$(".submit").on("click", function submitAnswer() {
+  var radios = document.getElementsByName("chord");
+  var i = 0,
+    len = radios.length;
+  var checked = false;
+  var userAnswer;
+
+  for (; i < len; i++) {
+    if (radios[i].checked) {
+      checked = true;
+      userAnswer = radios[i].value;
+    }
+  }
+  // Correct answer
+  if (userAnswer === "egb") {
+    correct++;
+    $("#right").text(correct);
+  }
+  // incorrect answer
+  else {
+    incorrect++;
+    $("#wrong").text(incorrect);
+  }
+});
+//submit change CSS
+$(".submit").on("click", function() {
+  $("#main-content").css("display", "none");
+  $(".score").css("display", "block");
+});
+
+//start game CSS
+$(".begin").on("click", function() {
+  $(".intro").css("display", "none");
+  $("#main-content").css("display", "block");
 });
